e986d863b65b9c791a9fd825a66f1a483476477b
ucsf-app <63319466+ucsf-app@users.noreply.github.com>
2023-07-05 18:26:25 -0300

    Archivos del Desafio 3

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const {ProductManager} = require ("./productManager");
+import express from "express";
+
+const path = "./products.json";
+const app = express ();
+const port = 8080;
+
+const prodManager = new ProductManager(path);
+
+app.listen(port,()=>{console.log(`El servidor esta escuchando en el puerto ${port}`)});
+
+app.get("/products", async(req,res)=>{
+    try {
+        let limit = req.query.limit;
+        let products = prodManager.getProduct();
+        let limitedProducts =[];
+        if (limit || limit >= products.length){
+            return await res.send (limitedProducts = products.slice(0,limit-1));
+        }else{
+            return await res.send(products);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+app.get("/products/:pid", async(req,res)=>{
+    try {
+        let productID = req.params.pid;
+        return await res.send(prodManager.getProductByID(productID));   
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+console.log("prodManager", prodManager);
---
+++ src/productManager.js
@@ -0,0 +1,116 @@
+const fs = require ("fs");
+
+class ProductManager {
+    constructor (path) {
+        this.path = path;
+        console.log(this.path);
+    };
+
+    contenido (){ 
+        return fs.readFileSync (this.path, "utf-8")
+    };
+    contenidoArreglo () {
+       return JSON.parse(this.contenido ());
+    };
+
+    writeToFile (contentToWrite) {
+            fs.writeFileSync (this.path,JSON.stringify(contentToWrite,null,"\t"));
+        };
+
+    addProduct (title, descritpion, price, thumbnail,code, stock){
+        let newID;
+        
+        const newProduct = {
+            id: 0,
+            title,
+            descritpion,
+            price,
+            thumbnail,
+            code,
+            stock
+        };
+
+        if (fs.existsSync (this.path)){       
+            const contentArray = this.contenidoArreglo ();
+            console.log("Estoy aca", contentArray);
+            newID = contentArray[contentArray.length - 1].id + 1;
+            newProduct.id = newID;
+            if ( contentArray.some ((productCode) => productCode.code === newProduct.code)){
+                console.log ("El valor de code esta repetido")
+            } else {
+                contentArray.push (newProduct);
+                console.log("El nuevo contenido es", contentArray);
+                this.writeToFile(contentArray);
+            };
+        } else {
+            const firstProduct = [];
+            newProduct.id = 1;
+            firstProduct.push (newProduct);
+            fs.writeFileSync (this.path, JSON.stringify(firstProduct,null,"\t"));
+        }
+    };
+
+    getProduct (){
+        if (fs.existsSync (this.path)){
+            const contentArray = this.contenidoArreglo ();
+            return contentArray;
+        } else {
+            return console.log ("No se puede getProduct porque el archivo no existe.");
+        }
+    };
+
+    getProductByID (productID) {
+        let productExist; 
+        
+        if (fs.existsSync (this.path)){
+            const contentArray = this.contenidoArreglo ();
+            productExist = contentArray.find((product)=> product.id === productID);
+            if (!productExist){
+                return ("Not Found");
+            } else {
+                return productExist;
+            }; 
+        } else {
+            return console.log ("No se puede getProductByID porque el archivo no existe.");
+        }
+    };
+
+    updateProduct (productID, campoActualizar, valorActualizar){
+        let productoActualizar;
+
+        if (fs.existsSync (this.path)){
+            const contenido = fs.readFileSync (this.path, "utf-8");
+            const contenidoArreglo = JSON.parse(contenido);
+            productExist = contenidoArreglo.indexOf((product)=> product.id === productID);
+                if (!productExist){
+                    return ("Not Found");
+                } else {
+                    productoActualizar = contenidoArreglo [productExist];
+                };
+            productoActualizar.campoActualizar = valorActualizar;
+            contenidoArreglo [productExist] = productoActualizar;
+            fs.writeFileSync(this.path, JSON.stringify(contenidoArreglo,null,"/t"));
+        } else {
+            return console.log ("No se puede updateProduct porque el archivo no existe.");
+        }
+    };
+
+    deleteProduct (productID){
+        if (fs.existsSync (this.path)){
+            const contenido = fs.readFileSync (this.path, "utf-8");
+            const contenidoArreglo = JSON.parse(contenido);
+            productExist = contenidoArreglo.indexOf((product)=> product.id === productID);
+                if (!productExist){
+                    return ("Not Found");
+                } else {
+                    contenidoArreglo.splice(productExist,1);
+                    fs.writeFileSync(this.path, JSON.stringify(contenidoArreglo,null,"/t"));        
+                };
+        } else {
+            return console.log ("No se puede updateProduct porque el archivo no existe.");
+        }
+    };
+
+}
+
+module.exports = { ProductManager }
