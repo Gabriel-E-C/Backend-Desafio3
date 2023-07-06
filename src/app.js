40c6cee08dbf007b82989336775d6abf90d122cc
ucsf-app <63319466+ucsf-app@users.noreply.github.com>
2023-07-05 22:27:03 -0300

    Segunda iteracion

--- src/app.js
+++ src/app.js
@@ -11,11 +11,13 @@ app.listen(port,()=>{console.log(`El servidor esta escuchando en el puerto ${por
 
 app.get("/products", async(req,res)=>{
     try {
+        console.log(req.query);
         let limit = req.query.limit;
+        console.log("El limite es",limit);
         let products = prodManager.getProduct();
         let limitedProducts =[];
         if (limit || limit >= products.length){
-            return await res.send (limitedProducts = products.slice(0,limit-1));
+            return await res.send (limitedProducts = products.slice(0,limit));
         }else{
             return await res.send(products);
         }
@@ -26,8 +28,10 @@ app.get("/products", async(req,res)=>{
 
 app.get("/products/:pid", async(req,res)=>{
     try {
-        let productID = req.params.pid;
-        return await res.send(prodManager.getProductByID(productID));   
+        let productID = parseInt(req.params.pid);
+        console.log(productID);
+        let product = prodManager.getProductByID(productID);
+        return await res.send(product);   
     } catch (error) {
         console.log(error.message);
     }
